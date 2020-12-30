4df9db7b57fa1e800ca0cb9fbe633507105feefc
Harsh Gupta <hg.harsh28@gmail.com>
2020-12-30 11:11:08 +0530

    Update Interface1.java
    connected to database

--- Interface1.java
+++ Interface1.java
@@ -1,22 +1,28 @@
-// class name - Interface1
+// class name - LoginPage
 package centralHospital;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.Font;
 import java.awt.Color;
-import javax.swing.JTextArea;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
+import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-public class Interface1 extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class LoginPage extends JFrame {
+
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -25,7 +31,7 @@ public class Interface1 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Interface1 frame = new Interface1();
+					LoginPage frame = new LoginPage();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,56 +43,115 @@ public class Interface1 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Interface1() {
-		getContentPane().setBackground(new Color(216, 191, 216));
+	public LoginPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0 , 0, 700, 800);
-		getContentPane().setLayout(null);
-		
-		JLabel lblNewLabel_1 = new JLabel("  A Comprehensive Healthcare..");
-		lblNewLabel_1.setForeground(new Color(255, 0, 0));
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 14));
-		lblNewLabel_1.setBounds(313, 56, 231, 73);
-		getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel = new JLabel("  +");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 53));
-		lblNewLabel.setForeground(new Color(255, 0, 0));
-		lblNewLabel.setBounds(0, 0, 99, 60);
-		getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_2 = new JLabel("Central Hospital");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 48));
-		lblNewLabel_2.setForeground(new Color(0, 0, 255));
-		lblNewLabel_2.setBounds(109, 0, 445, 73);
-		getContentPane().add(lblNewLabel_2);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 228, 225));
-		panel.setBounds(0, 84, 685, 666);
-		getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		
-		
-		JButton btnNewButton_1 = new JButton("View Status");
-		btnNewButton_1.setBounds(215, 397, 231, 40);
-		panel.add(btnNewButton_1);
-		btnNewButton_1.setBackground(new Color(255, 218, 185));
-		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		
-		JButton btnNewButton = new JButton("Register Patient");
-		btnNewButton.setBounds(215, 241, 231, 40);
-		panel.add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
+		setBounds(0, 0, 700, 800);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		Color colorBackground = new Color(0, 51, 102);
+		contentPane.setBackground(colorBackground);
+
+		JLabel loginLabel = new JLabel("Login");
+		loginLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 23));
+		loginLabel.setForeground(Color.WHITE);
+		loginLabel.setBounds(306, 168, 172, 55);
+		contentPane.add(loginLabel);
+
+		JLabel userNameLabel;
+		userNameLabel = new JLabel("Username(Mobile.no)");
+		userNameLabel.setForeground(Color.WHITE);
+		userNameLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		userNameLabel.setBounds(208, 229, 169, 39);
+		contentPane.add(userNameLabel);
+
+		JTextField usernameField;
+		usernameField = new JTextField();
+		usernameField.setColumns(10);
+		usernameField.setBounds(208, 266, 270, 32);
+		contentPane.add(usernameField);
+
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 15));
+		passwordLabel.setBounds(208, 308, 169, 29);
+		contentPane.add(passwordLabel);
+
+		JPasswordField passwordField;
+		passwordField = new JPasswordField();
+		passwordField.setBounds(208, 334, 270, 32);
+		contentPane.add(passwordField);
+
+		JButton loginButton = new JButton("Login");
+		loginButton.setBackground(Color.WHITE);
+		loginButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		loginButton.setBounds(296, 391, 81, 32);
+		contentPane.add(loginButton);
+		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//double click the button after that there is  new screen available
 				
-				new PatientDetails().setVisible(true);
+				Connection con ;
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+
+					String url = "jdbc:mysql://localhost:3306/central_hospital";
+					String dbusername = "root";
+					String dbpassword = "2802";
+
+					con = DriverManager.getConnection(url, dbusername, dbpassword);
+
+					String username = usernameField.getText();
+					String password = String.valueOf(passwordField.getPassword());
+
+					Statement stmt = con.createStatement();
+					String query = "select * from user_details where contact='" + username + "' and pass_word='" + password
+							+ "'";
+					
+					ResultSet rs = stmt.executeQuery(query);
+					
+					if(rs.next())
+					{
+						JOptionPane.showMessageDialog(contentPane,"Successfully Logined");  
+						dispose();
+						new Interface1().setVisible(true);
+						con.close();
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(contentPane,"wrong username or password");  
+						con.close();
+					}
+					
+					
+				}
+				
+				catch (Exception e2) {
+					System.out.println(e2.getMessage());
+				}
+			}
+		});
+
+		JLabel line2 = new JLabel("Don't have a account ? ");
+		line2.setForeground(Color.WHITE);
+		line2.setFont(new Font("Comic Sans MS", Font.PLAIN, 13));
+		line2.setBounds(272, 433, 149, 29);
+		contentPane.add(line2);
+
+		JButton signUpButton = new JButton("Sign up");
+		signUpButton.setForeground(Color.WHITE);
+		signUpButton.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		signUpButton.setBackground(colorBackground);
+		signUpButton.setBounds(296, 461, 83, 29);
+		contentPane.add(signUpButton);
+		signUpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// contentPane.setBackground(Color.YELLOW);
+				new RegistrationPage().setVisible(true);
+
 				dispose();
 			}
 		});
-		btnNewButton.setBackground(new Color(255, 218, 185));
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
+
 	}
 }
